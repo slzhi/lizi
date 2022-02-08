1ae3539ce766a691beb88c60be0b4ca24b63130d
slzhi <99185515+slzhi@users.noreply.github.com>
2022-02-08 21:28:07 +0800

    今日练习
    方便巩固

---
+++ code/Test.java
@@ -0,0 +1,12 @@
+
+/*
+	要求：请使用一句输出语句，达到输入如下效果
+	书名	作者	价格	销量
+	三国	罗贯中	120		1000
+*/
+public class Test{
+	
+	public static void main(String[] args){
+		System.out.println("书名\t作者\t价格\t销量\t\n三国\t罗贯中\t120\t1000");
+	}
+}
